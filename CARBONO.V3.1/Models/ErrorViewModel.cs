f0fa12412846231b48f2d8acfe84810aec3e9cd6
Yvani <yvanilemes@gmail.com>
2026-05-13 16:04:28 -0300

    Removendo pastas e arquivos não utilizados

--- CARBONO.V3.1/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CARBONO.V3._1.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
